fb1f03192532a15f57a609781cf784e0bbb04767
nanakusa <k.divocmkva.147@gmail.com>
2017-07-25 14:06:44 +0900

    新規作成

---
+++ AdapterPattern-Delegation/AdapterPattern-Delegation/Banner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern_Delegation
+{
+    public class Banner
+    {
+        private String str;
+        public Banner(String str)
+        {
+            this.str = str;
+        }
+        public void ShowWIthParen()
+        {
+            Console.WriteLine("(" + str + ")");
+        }
+        public void ShowWithAster()
+        {
+            Console.WriteLine("*" + str + "*");
+        }
+    }
+}
---
+++ AdapterPattern-Delegation/AdapterPattern-Delegation/PrintBanner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern_Delegation
+{
+    public class PrintBanner : Print
+    {
+        private Banner banner;
+        public PrintBanner(String str)
+        {
+            banner = new Banner(str);
+        }
+
+        public override void PrintWeak()
+        {
+            banner.ShowWIthParen();
+        }
+        public override void PrintStrong()
+        {
+            banner.ShowWithAster();
+        }
+    }
+}
